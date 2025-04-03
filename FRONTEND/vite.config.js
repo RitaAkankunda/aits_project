150f3c5769e499c7d449234cb71e85ab61a86da4
Rita Akankunda <akankundarita04@gmail.com>
2025-04-03 20:51:12 +0300

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aits_project/frontend/', // Adjust this based on your actual repo name
 })
